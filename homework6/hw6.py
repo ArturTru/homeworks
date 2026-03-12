b0d8c8eff5b27c62925ae368ab8b43e647c6af5f
Artur <47522820+ArturTru@users.noreply.github.com>
2026-03-12 21:03:28 +0300

    Replace tabs with spaces and shorten docstring

--- homework6/hw6.py
+++ homework6/hw6.py
@@ -1,22 +1,22 @@
 # homework6
 
-"""Module containing functions for homework 6."""
+"""homework 6."""
 
 
 def hw6_repl():
-    """Replace '#' with '/' in a predefined URL string."""
+    """Replace '#'"""
     my_str = "www.my_site.com#about"
     str_new = my_str.replace("#", "/", 1)
     return str_new
 
 
 def hw6_add_ing(word):
-    """Add 'ing' to the end of the given word."""
+    """Add 'ing'"""
     return word + "ing"
 
 
 def hw6_swap_str():
-    """Swap first and last name in the string 'Ivanou Ivan'."""
+    """Swap first and last name"""
     my_str = "Ivanou Ivan"
     my_list = my_str.split()
     result = ' '.join(my_list)
@@ -24,36 +24,33 @@ def hw6_swap_str():
 
 
 def hw6_remove_whitespace(text):
-    """Remove extra whitespace from the given text."""
+    """Remove whitespace"""
     return ' '.join(text.split())
 
 
 def hw6_fix_name(name):
-    """Capitalize the name properly (first letter uppercase, rest lowercase)."""
+    """Capitalize"""
     return name.capitalize()
 
 
 def hw6_str_to_list(word):
-    """Split a string into a list of words."""
+    """Split a string"""
     return word.split()
 
 
 def hw6_format_greeting(names, greeting, place):
-    """Return a formatted greeting string.
-
-    Uses a list with one name.
-    """
+    """Return a greeting"""
     name = names[0]
     return f"Hello, {name}! {greeting} to {place}"
 
 
 def hw6_list_to_string(lst):
-    """Convert a list of strings into a single string joined by spaces."""
+    """Convert a list"""
     return ' '.join(lst)
 
 
 def process_list(lst, new_value):
-    """Insert new value at position 3 and delete element at index 6."""
+    """Insert new value"""
     lst.insert(2, new_value)
     del lst[6]
     return lst
