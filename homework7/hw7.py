f13ff6ee3de9047544a74f7371916fdf4146dc45
Artur <47522820+ArturTru@users.noreply.github.com>
2026-03-17 21:09:02 +0300

    Add homework7

---
+++ homework7/hw7.py
@@ -0,0 +1,46 @@
+# homework7
+
+"""homework 7."""
+from operator import truediv
+
+from mypy.typeops import false_only
+
+
+def hw7_1(a):
+    """BRIDGES"""
+    t = abs(a)
+    h = (t // 60) % 24
+    m = t % 60
+    dig_h = [int(d) for d in str(h)]
+    dig_m = [int(m) for m in str(m)]
+    return sum(dig_h) + sum(dig_m)
+
+
+def hw7_2(b):
+    """EXP"""
+    exp = 10
+    new_exp = b + exp
+    lv_dict ={1:15, 2:25, 3:35, 4:45}
+    for level, required in lv_dict.items():
+        if new_exp == required:
+            return True
+    return False
+
+
+def hw7_3(h,m):
+    """HOURS"""
+    if h < 12:
+        period = "a.m."
+    else:
+        period = "p.m."
+
+
+    if h == 0 or h == 12:
+        hours = 12
+    elif h > 12:
+        hours = h - 12
+    else :
+        hours = h
+
+    time = f"{hours}:{m:02d} {period}"
+    return time
