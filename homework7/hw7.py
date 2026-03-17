94f03045a9b1e438f3b9c366dd9624fdb243a111
Artur <47522820+ArturTru@users.noreply.github.com>
2026-03-17 21:22:19 +0300

    Fix PEP8 issues: whitespaces, remove unused imports

--- homework7/hw7.py
+++ homework7/hw7.py
@@ -1,9 +1,6 @@
 # homework7
 
-"""homework 7."""
-from operator import truediv
-
-from mypy.typeops import false_only
+"""homework 7"""
 
 
 def hw7_1(a):
@@ -20,26 +17,25 @@ def hw7_2(b):
     """EXP"""
     exp = 10
     new_exp = b + exp
-    lv_dict ={1:15, 2:25, 3:35, 4:45}
+    lv_dict = {1: 15, 2: 25, 3: 35, 4: 45}
     for level, required in lv_dict.items():
         if new_exp == required:
             return True
     return False
 
 
-def hw7_3(h,m):
+def hw7_3(h, m):
     """HOURS"""
     if h < 12:
         period = "a.m."
     else:
         period = "p.m."
 
-
     if h == 0 or h == 12:
         hours = 12
     elif h > 12:
         hours = h - 12
-    else :
+    else:
         hours = h
 
     time = f"{hours}:{m:02d} {period}"
